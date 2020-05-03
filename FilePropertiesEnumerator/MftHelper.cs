0d62187271536f7644c8606528bc7a36794d9ace
AdamWhite <adamwhitehat@outlook.com>
2020-05-03 15:55:09 -0600

    Now scans and includes Hidden, System, Offline, Temporary.

--- FilePropertiesEnumerator/MftHelper.cs
+++ FilePropertiesEnumerator/MftHelper.cs
@@ -7,31 +7,27 @@ using System.IO.Filesystem.Ntfs;
 
 namespace FilePropertiesEnumerator
 {
-	using NtfsNodeAttributes = System.IO.Filesystem.Ntfs.Attributes;
+    using NtfsNodeAttributes = System.IO.Filesystem.Ntfs.Attributes;
 
-	public static class MftHelper
-	{
-		public static IEnumerable<INode> EnumerateMft(DriveInfo driveToAnalyze)
-		{
-			NtfsReader ntfsReader = new NtfsReader(driveToAnalyze, RetrieveMode.All);
+    public static class MftHelper
+    {
+        public static IEnumerable<INode> EnumerateMft(DriveInfo driveToAnalyze)
+        {
+            NtfsReader ntfsReader = new NtfsReader(driveToAnalyze, RetrieveMode.All);
 
-			IEnumerable<INode> nodes =
-				ntfsReader.GetNodes(driveToAnalyze.Name)
-					.Where(n => (n.Attributes &
-								 (
-									NtfsNodeAttributes.Hidden
-								  | NtfsNodeAttributes.System
-								  | NtfsNodeAttributes.Temporary
-								  | NtfsNodeAttributes.Device
-								  | NtfsNodeAttributes.Directory
-								  | NtfsNodeAttributes.Offline
-								  | NtfsNodeAttributes.ReparsePoint
-								  | NtfsNodeAttributes.SparseFile
-								 )
-								) == 0); // This means that we DONT want any matches of the above NtfsNodeAttributes types.
-					//.OrderByDescending(n => n.Size);
+            IEnumerable<INode> nodes =
+                ntfsReader.GetNodes(driveToAnalyze.Name)
+                    .Where(n => (n.Attributes &
+                                 (
+                                    NtfsNodeAttributes.Device
+                                  | NtfsNodeAttributes.Directory
+                                  | NtfsNodeAttributes.ReparsePoint
+                                  | NtfsNodeAttributes.SparseFile
+                                 )
+                                ) == 0); // This means that we DONT want any matches of the above NtfsNodeAttributes types.
+                                         //.OrderByDescending(n => n.Size);
 
-			return nodes;
-		}
-	}
+            return nodes;
+        }
+    }
 }
